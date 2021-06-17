a32878deff412252bdc7296a1e008877253ad8df
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-06-17 15:10:25 +0200

    used function references to pass on action handlers to child widget Answer

---
+++ lib/answer.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  Answer(this.selectHandler);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        child: Text('Answer 1'),
+        onPressed: selectHandler,
+        color: Colors.blue,
+        textColor: Colors.white,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -16,7 +18,7 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-    
+
     print('answerQuestion() function ran...' + _questionIndex.toString());
   }
 
@@ -35,21 +37,24 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(_questions[_questionIndex]),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('Answer 2 chosen!'),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                //...
-                print('Answer 3 chosen!');
-              },
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            // RaisedButton(
+            //   child: Text('Answer 1'),
+            //   onPressed: _answerQuestion,
+            // ),
+            // RaisedButton(
+            //   child: Text('Answer 2'),
+            //   onPressed: () => print('Answer 2 chosen!'),
+            // ),
+            // RaisedButton(
+            //   child: Text('Answer 3'),
+            //   onPressed: () {
+            //     //...
+            //     print('Answer 3 chosen!');
+            //   },
+            // ),
           ],
         ),
       ),
